13ed5779c03477b7082c70d8ab9dfe39f94e4151
Angel <agamez6@unisimon.edu.co>
2022-03-01 00:07:36 -0500

    basicos

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // INTERPOLACION
+//   titulo = 'Vamos angelito tu puedes con esto';
+// url="https://pbs.twimg.com/media/FMtXH75XsAELkOg.jpg"
+
+// NGMODEL
+
+// name!: string;
+
+// NGFOR & NGIF
+
+// cities = ['Barranquilla','Cartagena', 'Santa Marta']
+
+// name!: string;
+// //////////////NGCLASS/////////////////////
+
+cities = ['Barranquilla','Cartagena', 'Santa Marta']
+
+name!: string;
+
+}
